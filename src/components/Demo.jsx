05c395b282ece222b97516c44dd15766949e67e3
Vasil <vaskodzhakov@gmail.com>
2023-06-17 21:01:58 +0100

    Input styling

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
 import { copy, linkIcon, loader, tick } from '../assets'
 import { GrNext } from 'react-icons/gr'
+import { MdManageSearch } from'react-icons/md'
+
+import { FaGithub, FaLink } from 'react-icons/fa'
+import { BsLink, BsLink45Deg } from 'react-icons/bs'
+import { CiLink } from 'react-icons/ci'
+import { HiLink } from 'react-icons/hi'
 export default function Demo() {
   const [article, setArticle] = useState({
     url: '',
@@ -14,8 +20,8 @@ export default function Demo() {
   return (
     <section className="mt-12 w-full max-w-xl">
       <div className="w-full-gap-2 flex flex-col">
-        <form onSubmit={handleSubmit} className="relative flex items-center justify-center">
-          <img src={linkIcon} alt="linkicon" className="absolute left-0 my-2 ml-3 w-5" />
+        <form onSubmit={handleSubmit} className="peer relative flex items-center justify-center">
+          {/* <img src={linkIcon} alt="linkicon" className="absolute left-0 my-2 ml-3 w-5" /> */}
           <input
             type="url"
             placeholder="Enter a link to an article"
@@ -25,9 +31,10 @@ export default function Demo() {
             }}
             className="peer block w-full rounded-xl border-2 border-gray-400 bg-[#fcfcff] p-1.5 px-3 py-2.5 pl-10 pr-12 font-satoshi text-sm font-medium shadow-lg transition-all hover:rounded-xl  hover:bg-white hover:pl-10 hover:text-black focus:bg-white"
           />
+          <HiLink className="absolute left-0 flex w-10 items-center justify-center rounded rounded-r-lg text-gray-400 hover:text-gray-800 peer-focus:text-gray-700 ml-1" size={25}/>
 
-          <button type="submit" className="submit_btn border-2  peer-focus:border-gray-700 peer-focus:text-gray-700">
-            <GrNext />
+          <button type="submit" className="submit_btn border-2 hover:bg-slate-200 peer-focus:border-gray-700 peer-focus:text-gray-700">
+            <MdManageSearch size={25}/>
           </button>
         </form>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
+import { MdOutlineSummarize } from 'react-icons/md'
+import { LuListTree, LuListEnd, LuListStart } from 'react-icons/lu'
+
+import { DiGithubFull } from 'react-icons/di'
+import { FaGithub, FaLink } from 'react-icons/fa'
+import { BsLink, BsLink45Deg } from 'react-icons/bs'
+import { CiLink } from 'react-icons/ci'
+import { HiLink } from 'react-icons/hi'
 
 export default function Hero() {
   return (
     <header className="flex w-full flex-col items-center justify-center">
       <nav className="mb-10 flex w-full items-center justify-between pt-3">
-        <img src={logo} className="w-24 object-contain" alt="logo" />
-
+        {/* <img src={logo} className="w-24 object-contain" alt="logo" /> */}
+        <div className="flex items-end">
+          <LuListTree size={40} />
+          <span className="ml-1 text-2xl font-extrabold">Sumz</span>
+        </div>
+        Sumz
         <button
           type="button"
           onClick={() => window.open('https://github.com/Vasil1001?tab=repositories&type=source')}
@@ -15,6 +27,15 @@ export default function Hero() {
           GitHub
         </button>
       </nav>
+      <LuListEnd size={40} />
+      <LuListStart size={40} />
+      <MdOutlineSummarize size={40} />
+
+      <FaLink size={40} />
+      <MdOutlineSummarize size={40} />
+
+
+
       <h1 className="mt-3 text-center text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl">
         Summarize <br className="max-2xl:hidden" />
         Article links <br className="max-md:hidden" />

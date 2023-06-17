7becab25109853bf3bd0cc5d6ec84f8c044f88a8
Vasil1001 <vaskodzhakov@gmail.com>
2023-06-17 21:56:09 +0100

    UI
    UI

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,6 +7,8 @@ import { FaGithub, FaLink } from 'react-icons/fa'
 import { BsLink, BsLink45Deg } from 'react-icons/bs'
 import { CiLink } from 'react-icons/ci'
 import { HiLink } from 'react-icons/hi'
+import { LuListEnd } from 'react-icons/lu'
+
 export default function Demo() {
   const [article, setArticle] = useState({
     url: '',
@@ -29,12 +31,14 @@ export default function Demo() {
             onChange={(e) => {
               setArticle({ ...article, url: e.target.value })
             }}
-            className="peer block w-full rounded-xl border-2 border-gray-400 bg-[#fcfcff] p-1.5 px-3 py-2.5 pl-10 pr-12 font-satoshi text-sm font-medium shadow-lg transition-all hover:rounded-xl  hover:bg-white hover:pl-10 hover:text-black focus:bg-white"
+            className="peer block w-full rounded-xl border-2 border-gray-400 bg-[#fcfcff] p-1.5 px-3 py-2.5 pl-10 pr-12 font-satoshi text-md font-medium shadow-lg transition-all hover:rounded-xl  hover:bg-white hover:pl-10 hover:text-black focus:bg-white"
           />
           <HiLink className="absolute left-0 flex w-10 items-center justify-center rounded rounded-r-lg text-gray-400 hover:text-gray-800 peer-focus:text-gray-700 ml-1" size={25}/>
 
           <button type="submit" className="submit_btn border-2 hover:bg-slate-200 peer-focus:border-gray-700 peer-focus:text-gray-700">
-            <MdManageSearch size={25}/>
+            {/* <MdManageSearch size={25}/> */}
+            <LuListEnd size={25} />
+
           </button>
         </form>
 
